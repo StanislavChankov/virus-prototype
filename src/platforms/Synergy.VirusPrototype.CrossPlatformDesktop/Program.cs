2f826438d400ea63d178a56d44eed5213ff1b802
Stanislav Chankov <stanislavchankov94@gmail.com>
2020-10-08 18:46:59 +0300

    * Initial architecture;

---
+++ src/platforms/Synergy.VirusPrototype.CrossPlatformDesktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using Synergy.VirusPrototype.Shared;
+
+namespace Synergy.VirusPrototype.CrossPlatformDesktop
+{
+    public static class Program
+    {
+        //[STAThread]
+        static void Main()
+        {
+            using (var game = new VirusPrototypeGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
